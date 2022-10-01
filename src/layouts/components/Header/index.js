228c8b52e868052cd1630b15e0b65b6ff04a833b
ngdkhuong <nguyenkhuong31072002@gmail.com>
2022-10-01 12:08:03 +0700

    update source code 01/10 12:05

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -15,7 +15,7 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { Link } from 'react-router-dom';
 
-import routesConfig from '~/config/routes';
+import config from '~/config';
 import Menu from '~/components/Popper/Menu';
 import Button from '~/components/Button';
 import styles from './Header.module.scss';
@@ -91,7 +91,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img src={images.logo} alt="tiktok"></img>
                 </Link>
 
